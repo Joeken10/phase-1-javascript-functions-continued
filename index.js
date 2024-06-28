811916c0cc366045d88e1b229bb126401b825f35
Joeken10 <joekentafir@gmail.com>
2024-06-28 15:51:53 +0300

    add new functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(VisualFlair = "*") {
+  return function(adjective) {
+      return `You are ${VisualFlair}${adjective}${VisualFlair}!`;
+  }
+}
